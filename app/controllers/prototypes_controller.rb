ce7b1eab0d5059674f8cba113383c7aefabd3185
midor030 <itigodaifuku030@icloud.com>
2020-11-10 23:26:21 +0900

    詳細ページ編集クリア！

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,11 +23,18 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
   end
-  
+
   private
   def move_to_index
     unless user_signed_in?
