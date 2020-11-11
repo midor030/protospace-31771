effe21f614181f4da2ffca33f34ff47455225214
midor030 <itigodaifuku030@icloud.com>
2020-11-11 14:41:21 +0900

    コントローラーにストロングパラメーター設置クリア！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    
   end
+
+  private
+  def comment_params
+  params.require(:comment).permit(:title).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+
 end
