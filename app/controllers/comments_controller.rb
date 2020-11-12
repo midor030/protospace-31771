bb17e33ab1ff349e15edc7f6a7aa0997760410f5
midor030 <itigodaifuku030@icloud.com>
2020-11-12 17:13:44 +0900

    バグ？修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,6 @@ class CommentsController < ApplicationController
     end
   end
 
-
   private
   def comment_params
    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
